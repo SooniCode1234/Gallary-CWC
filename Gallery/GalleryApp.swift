0b0c924c186f077bd4ca2756f323c30a10509a58
Sooni Mohammed <sooni.dev@gmail.com>
2022-07-06 19:03:37 -0400

    Initial Commit

---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Sooni Mohammed on 2022-07-06.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
